fcbab4ee7069f1b1c688a63244a9cbe9c1516e34
Luis Filippe Ullmann <filippemkt@gmail.com>
2021-04-08 19:50:51 -0300

    Players

---
+++ js.js
@@ -0,0 +1,22 @@
+var jogador1 = {
+  nome: "Personagem1",
+  ataque: 80,
+  defesa: 60,
+  magia: 90,
+};
+
+var jogador2 = {
+  nome: "Personagem2",
+  ataque: 70,
+  defesa: 65,
+  magia: 85,
+};
+
+var jogador3 = {
+  nome: "Personagem3",
+  ataque: 88,
+  defesa: 62,
+  magia: 90,
+};
+
+var cartas = [cartaJogador1, cartaJogador2, cartaJogador3];
